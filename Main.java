2c7abfdd7522e490e4147679b8e1e2ad2c4e1e5b
Renusree T <kanchanatharani@gmail.com>
2024-12-01 20:35:20 +0530

    Main.java

---
+++ Main.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+
+public class Main {  // Make sure this matches the class name exactly
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();  // Read input
+        System.out.println(a); // Output the integer
+        sc.close();  // Close the scanner
+    }
+}
